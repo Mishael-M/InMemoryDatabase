0291e23b9abee3f35ff7a1d20ebf0d48c895078d
Mishael <magsanoc@live.com>
2023-02-22 16:31:57 -0500

    Created constructor and put method for in memory database

--- src/com/database/KeyValueStore.java
+++ src/com/database/KeyValueStore.java
@@ -1,6 +1,27 @@
 package com.database;
 
-public class KeyValueStore {
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyValueStore<K, V> {
+	private Map<K, V> databaseMap;
+	
+	public KeyValueStore() {
+		
+		
+		databaseMap = new HashMap<>();
+	}
+	
+	/**
+	 * Adds the key value to hashmap
+	 * @param key
+	 * @param value
+	 * @return value
+	 */
+	public V put(K key, V value) {
+		this.databaseMap.put(key, value);
+		return value;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
